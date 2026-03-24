b24d42bb35f6283043dd9fdffe442dc300cc188c
jakubmajda <jakub.majda@student.wsb.edu.pl>
2026-03-24 20:42:37 +0100

    Add files via upload

---
+++ SredniaC#.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbe ocen:");
+        float grades = float.Parse(Console.ReadLine());
+        float avg=0;
+
+        for (int i = 0; i < grades; i++)
+        {
+            Console.WriteLine("Podaj ocene:");
+            avg = avg + float.Parse(Console.ReadLine());
+        }
+
+        avg = avg / grades;
+        Console.WriteLine("Srednia ocen:");
+        Console.WriteLine(avg);
+        if(avg<3)
+        {
+            Console.WriteLine("Nie zdales");
+        }
+        else if (avg>3)
+        {
+            Console.WriteLine("Zdales");
+        }    
+
+    }
+}
