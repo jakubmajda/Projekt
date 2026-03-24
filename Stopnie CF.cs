20c7b99627d56caa100e6840971444ed319298a6
jakubmajda <jakub.majda@student.wsb.edu.pl>
2026-03-24 20:16:17 +0100

    Add files via upload

---
+++ Stopnie CF.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbe stopni:");
+        float degrees = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Wybierz skale w ktorej sa twoje stopnie:");
+        Console.WriteLine("1 - Stopnie C");
+        Console.WriteLine("2 - Stopnie F");
+        int action = int.Parse(Console.ReadLine());
+        if (action==1)
+        {
+            Console.WriteLine("Wartosc po konwersji na F:");
+            Console.WriteLine(degrees * 1.8 + 32); 
+        }
+        else if (action==2)
+        {
+            Console.WriteLine("Wartosc po konwersji na C:");
+            Console.WriteLine((degrees - 32) / 1.8);
+        }
+
+
+    }
+}
